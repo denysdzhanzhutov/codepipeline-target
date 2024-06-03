313450bf968463cb313d4ddb29b8f5df706f6add
Denys Dzhanzhutov <denys.dzhanzhutov@gmail.com>
2024-06-03 10:43:05 -0600

    ~

---
+++ src/test/java/com/example/codepipeline_target/CodepipelineTargetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codepipeline_target;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepipelineTargetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
